14c43e1e85b0e998cc56735ce2a518e1350d64a8
lexysurya <lexy.hutabarat@gmail.com>
2022-04-06 20:38:14 +0700

    commit

---
+++ application/models/Model_Biodata
@@ -0,0 +1,11 @@
+<?php
+
+class Model_biodata extends CI_Model{
+
+    public function biodata()
+    {
+    $hasil ="Lexy Surya Hutabarat";
+    return $hasil;
+    }
+
+}
---
+++ application/views/V_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Lexy</li>
+        <li>Nama Belakang : surya</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jl. satrian 9 no 99 rt 07 rw 06</li>
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Bekasi</li>
+    </ul>
+    
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Berenang</li>
+    </ul>
+</section>
